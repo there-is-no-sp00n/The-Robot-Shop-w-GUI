93be317998e17c95708e817aaf84aa5a8bb7a009
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-11-10 02:01:45 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,14 +16,5 @@ int main()
 	View obj;
 	obj.show_the_goods();
 
-	//Fl_Window *win = new Fl_Window(340, 180, "Robot Shop Manager");
-
-	//Fl_Box *box = new Fl_Box(20, 30, 300, 100, "Hello");
-
-	//Fl_Menu_Bar *menubar = new Fl
-
-	//win->end();
-	//win->show();
-
-	return 0;//Fl::run();
+	return 0;
 }
